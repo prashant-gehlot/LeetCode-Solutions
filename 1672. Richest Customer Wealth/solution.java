82fcfa82dbf437d536b88a42eba57158cfa0297c
Prashant Gehlot <98162883+prashant-gehlot@users.noreply.github.com>
2022-11-24 09:20:52 +0530

    Create solution.java

---
+++ 1672. Richest Customer Wealth/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        // person = row
+        // account = col
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int person = 0; person < accounts.length ; person++) {
+            // when you start a new col, take a new sum fot that row
+            int rowSum = 0;
+            for (int account = 0; account < accounts[person].length ; account++) {
+                rowSum = rowSum + accounts[person][account];
+            }
+            if (rowSum > maxSum) {
+                maxSum = rowSum;
+            }
+        }
+        return maxSum;
+    }
+}
