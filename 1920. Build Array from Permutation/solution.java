a8368c77e6a412db37252528b4078c172b2a9d6a
Prashant Gehlot <98162883+prashant-gehlot@users.noreply.github.com>
2022-11-24 09:17:39 +0530

    Create solution.java

---
+++ 1920. Build Array from Permutation/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int size = nums.length;
+        int[] ans = new int[size];
+        
+        for (int i =0; i <= size-1; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
